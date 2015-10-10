c4a7b2620c17b6d1fd01760c235ee922c4c7cc04
RobertZK <technoguyrob@gmail.com>
2015-10-10 00:35:52 -0500

    Initial trivial package.

---
+++ src/Data/Matrix.hs
@@ -0,0 +1,6 @@
+module Data.Matrix (
+  matrix
+) where
+
+import Data.Matrix.Construct
+
---
+++ src/Data/Matrix/Construct.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE DataKinds #-} 
+
+module Data.Matrix.Construct (
+  matrix
+) where
+
+matrix :: Num a => a
+matrix = 1
+
